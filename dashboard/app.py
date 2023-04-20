c731b0fda7fadbe4f447cc7bb8c6b655ed54bcdb
sachimugu <sachimugu@outlook.com>
2023-04-20 15:15:52 +0100

    dashboard

---
+++ dashboard/app.py
@@ -0,0 +1,7 @@
+
+# dash dependencies
+from dash_extensions.enrich import  DashProxy, MultiplexerTransform, html,  dcc
+import dash
+import dash_bootstrap_components as dbc
+from dash import html
+import pandas as pd
